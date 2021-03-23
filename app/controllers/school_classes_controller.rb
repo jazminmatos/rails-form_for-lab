6b9243794a55ba1a372e473ccb04f9a0bf0c1771
jazminmatos <jmatos1993@gmail.com>
2021-03-23 12:20:34 -0400

    commit tree 44a091a01be76a67fe4454ba4b3ab604db416adb

---
+++ app/controllers/school_classes_controller.rb
@@ -0,0 +1,32 @@
+class SchoolClassesController < ApplicationController
+    def index
+    end
+    
+    
+    def new
+        @school_class = SchoolClass.new
+    end
+
+    def create
+        @school_class = SchoolClass.new(school_class_params)
+        @school_class.save
+        redirect_to school_class_path(@school_class)
+    end
+
+    def show
+        @school_class = SchoolClass.find(params[:id])
+    end
+
+    def edit
+        @school_class = SchoolClass.new 
+    end
+
+    def update
+    end
+
+    private
+
+    def school_class_params
+        params.require(:school_class).permit(:title, :room_number)
+    end
+end
